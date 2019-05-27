72de27cebbe7c45c6e917b7cbba93e6a329a7f17
ikusmart <kusmartsev.ivan@gmail.com>
2019-05-28 02:10:28 +0300

    Added file and interactive modes for console application

---
+++ CanonicalEquation/CanonicalEquation.App/Options.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
+
+namespace CanonicalEquation.App
+{
+    public class Options
+    {
+        public enum EMode
+        {
+            Unknown = -1,
+            Interactive = 1,
+            File = 2
+        }
+
+        [Usage(ApplicationAlias = "CanonicalEquation.App")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("Interactive mode", new Options { Mode = EMode.Interactive });
+                yield return new Example("File mode", new Options { Mode = EMode.File, InputFile = "inputFile.in" });
+            }
+        }
+
+        [Option('m', "mode", Required = true, HelpText = "Mode: 'interactive' (or 1) or 'file' (or 2)  modes")]
+        public EMode Mode { get; set; }
+
+        [Option('f', "file", Required = false, HelpText = "Input file name")]
+        public string InputFile { get; set; }
+    }
+}
--- CanonicalEquation/CanonicalEquation.App/Program.cs
+++ CanonicalEquation/CanonicalEquation.App/Program.cs
@@ -1,12 +1,103 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using CanonicalEquation.Lib;
+using CanonicalEquation.Lib.Extensions;
+using CanonicalEquation.Lib.Parsers;
+using CommandLine;
 
-namespace CanonicalEquation.Lib.App
+namespace CanonicalEquation.App
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var parser = new Parser(cfg => cfg.CaseInsensitiveEnumValues = true);
+            return parser.ParseArguments<Options>(args)
+                .MapResult(HandleRunWithSelectedMode, errs => 1);
+        }
+
+        private static int HandleRunWithSelectedMode(Options arg)
+        {
+            try
+            {
+                switch (arg.Mode)
+                {
+                    case Options.EMode.Interactive:
+                        return RunInteractiveMode();
+                    case Options.EMode.File:
+                        return RunFileMode(arg.InputFile);
+                    default:
+                        throw new ArgumentException("Unknown working mode. The application will be stopped");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                return 1;
+            }
+        }
+
+        private static int RunFileMode(string inputFileName)
+        {
+            var totalLinesProcessed = 0;
+            var inputLines = File.ReadAllLines(inputFileName);
+
+            var outputLines = inputLines.Select((inputLine, i) =>
+            {
+                var parseResult = EquationParser.TryParse(inputLine, out var equation);
+                if (parseResult.IsSucceed)
+                {
+                    totalLinesProcessed++;
+                    return equation.ToCanonicalEquation().ToString();
+                }
+                else
+                {
+                    return $"Failed to parse line {i}: {inputLine}";
+                }
+            });
+
+            File.WriteAllLines($"{inputFileName}.out", outputLines);
+            Console.WriteLine($"{totalLinesProcessed} lines processed");
+
+            return 0;
+        }
+
+        private static int RunInteractiveMode()
+        {
+
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
+            {
+                var isCtrlC = e.SpecialKey == ConsoleSpecialKey.ControlC;
+                // Prevent CTRL-C from terminating
+                if (isCtrlC)
+                {
+                    e.Cancel = true;
+                }
+                // e.Cancel defaults to false so CTRL-BREAK will still cause termination
+            };
+
+            Console.WriteLine("Interactive mode: enter equations and displays result on enter. For exit press Ctrl+C");
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("Enter equation: ");
+                string line = Console.ReadLine();
+
+                var parseResult = EquationParser.TryParse(line, out var equation);
+                if (parseResult.IsSucceed)
+                {
+                    Console.WriteLine("Canonical equation: " + equation.ToCanonicalEquation());
+                }
+                else
+                {
+                    Console.WriteLine($"Equation parse failed. See the errors: {parseResult.Errors.CollectionToStringWithSeparator(Environment.NewLine)}");
+                }
+            }
+
+
         }
     }
 }
